09b71812678d51ffb1867a38cb641fbca248b553
Guilherme130499 <guilherme.s.correia@iesb.edu.br>
2025-05-06 19:44:52 -0300

    Aula Stack

---
+++ aula09/src/routes/StackRoutes.jsx
@@ -0,0 +1,27 @@
+import {createStackNavigator} from "@react-navigation/stack"
+import ProfileScreen from '../screens/ProfileScreen'
+import HomeScreen from "../screens/HomeScreen"
+import ConfigScreen from "../screens/ConfigScreen"
+
+
+const Stack = createStackNavigator()
+
+
+export default function StackRoutes() {
+  return (
+            <Stack.Navigator>
+                <Stack.Screen 
+                    name="HomeScreen"
+                    component={HomeScreen}
+                    options={{
+                        title:"Inicio",
+                        headerTitleAlign: "center",                        
+                    }}/>
+
+
+               
+                <Stack.Screen name='ProfileScreen' component={ProfileScreen} />
+                <Stack.Screen name='configScreen' component={ConfigScreen} />
+            </Stack.Navigator>
+  )
+}

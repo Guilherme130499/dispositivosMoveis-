5ebde4416782ed7a76567a36ab4bfb3777e7112b
Guilherme130499 <guilherme.s.correia@iesb.edu.br>
2025-05-23 21:42:13 -0300

    Lista Usuarios

--- 12-apis/src/routes/screens/UsuarioScreens.jsx
+++ 12-apis/src/routes/screens/UsuarioScreens.jsx
@@ -1,15 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Avatar, Card, Text } from 'react-native-paper'
+import { Axios } from 'axios'
 
 export default function UsuarioScreens({ navigation, route }) {
   
+  const idUsuario = route.params
+  const [usuario, setUsuario] = useState({})
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    axios.get("" + idUsuario)
+      .then(resposta => {
+        setUsuario(resposta.data)
+      })
+  })
   
-  console.log("ID DO USUARIO RECEBIDO: ", route.params)
   
   
     return (
     <View>
-      <Text>UsuarioScreens</Text>
+      <Card>
+        <Card.Title 
+          title={usuario.firstName + " " + usuario.lastName}
+          subtitle={usuario.email}
+          left={(props) => <Avatar.Image {...props} source={{uri: usuario.image}}/>}
+        /> 
+        <Card.Content>
+            <Text variant='titleLarge'>Dados</Text>
+            <Text>Username:</Text>
+            <Text>Idade:</Text>
+            <Text>Gênero:</Text>
+            <Text>Telefone:</Text>
+            <Text>Data de nascimento:</Text>
+            <Text>Universidade:</Text>
+        </Card.Content>
+      </Card>
     </View>
   )
 }

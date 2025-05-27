5fb910faae9e2f30f5ce31979c2565e811382447
Guilherme130499 <guilherme.s.correia@iesb.edu.br>
2025-05-27 20:06:28 -0300

    Exercicio loja

--- exercicio-loja/App.js
+++ exercicio-loja/App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import StackRoutes from './src/routes/StackRoutes';
+import { PaperProvider } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+        <StackRoutes />
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
---
+++ exercicio-loja/src/routes/StackRoutes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { createStackNavigator } from '@react-navigation/stack'
+
+import HomeScreens from '../screens/HomeScreens'
+import ListaProdutosScreen from '../screens/ListaProdutosScreen'
+import ProdutoScreen from '../screens/ProdutoScreen'
+
+const Stack = createStackNavigator()
